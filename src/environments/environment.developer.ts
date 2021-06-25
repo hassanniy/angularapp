ab96999b9d758a13778cbda879fa0ca5690f77c5
Johnny Bravo <musiccworlddd@gmail.com>
2021-06-25 15:17:14 +0500

    first commit

---
+++ src/environments/environment.developer.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  hostURL: 'http://dev.site',
+  username: 'DevelopmentUser',
+  token: 'DevelopmentToken',
+  port: 8080,
+  envName: 'dev'
+};
+  
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  hostURL: 'http://prod.site',
+  username: 'ProductionUser',
+  token: 'ProductionToken',
+  port: 8080,
+  envName: 'prod'
+};
